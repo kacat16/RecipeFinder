fddfcdef5dc154bf6c43a719fa4edd323df9b925
kacat16 <160745157+kacat16@users.noreply.github.com>
2024-05-19 23:01:10 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecipeController;
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::get('/recipe_search', function(){
+    return view('recipes.search');
+})->name('recipe_search');
+
+Route::get('/recipes/search',[RecipeController::class,'search'])->name('recipes.search');
+Route::get('/recipes/display/{id}',[RecipeController::class,'display'])->name('recipes.display');
+
+Route::post('/add_favourites',[RecipeController::class,'addToFavourites']);
+Route::post('/add_recipe',[RecipeController::class,'addToRecipes']);
+
+Route::get('/recipes/favourites',[RecipeController::class,'displayFavourites']);
